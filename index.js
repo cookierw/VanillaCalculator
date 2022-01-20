11c316a891e204ec5de35d8ab425b0db17f7bbf1
Sean W <seanrwhitley@gmail.com>
2022-01-20 08:21:00 -0800

    Update evaluation function (thanks Ethan)

--- index.js
+++ index.js
@@ -20,19 +20,16 @@ document.getElementById("equals")
 /* Event handlers */
 function handleDigitEvent(event) {
     let digit = event.target.innerText;
-    if (!display.innerText.match(/\d$/g)) {
-        digit = " " + digit;
-    }
     display.innerText += digit;
 }
 
 function handleActionEvent(event) {
     let action = event.target.innerText;
-    display.innerText += " " + action;
+    display.innerText += action;
 }
 
 function handleEvaluateEvent() {
-    display.innerText = ` ${evaluate(display.innerText)}`
+    display.innerText = `${evaluate(display.innerText)}`
 }
 
 /** Helper functions */
@@ -40,23 +37,7 @@ function clearDisplay() {
     display.innerText = "";
 }
 
+// Credit to Ethan
 function evaluate(string) {
-    let arr = string.split(" ");
-
-    // parseint() needed for addition
-    let a = parseInt(arr[0]);
-    let b = parseInt(arr[2]);
-    
-    switch (arr[1]) {
-        case '/':
-            return a / b;
-        case '*':
-            return a * b;
-        case '-':
-            return a - b;
-        case '+':
-            return a + b;
-        default:
-            break;
-    }
+    return new Function("return " + string)();
 }
