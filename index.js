6cc095a34bb6717dcfb23b6eafd8d59c036f7cff
Sean W <seanrwhitley@gmail.com>
2022-01-19 23:47:08 -0800

    Working calculator. Integers only for right now

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+/* Buttons! */
+const digits = document.getElementsByClassName("calc-digit");
+const digitsArr = Array.prototype.slice.call(digits);
+digitsArr.forEach(element => {
+    element.addEventListener("click", handleDigitEvent)
+});
 
+const actions = document.getElementsByClassName("action-button");
+const actionsArr = Array.prototype.slice.call(actions);
+actionsArr.forEach(element => {
+    element.addEventListener("click", handleActionEvent)
+});
 
-// Build equation string with trailing spaces
+document.getElementById("clear")
+    .addEventListener("click", clearDisplay);
+
+document.getElementById("equals")
+    .addEventListener("click", handleEvaluateEvent)
+
+/* Event handlers */
+function handleDigitEvent(event) {
+    let digit = event.target.innerText;
+    if (!display.innerText.match(/\d$/g)) {
+        digit = " " + digit;
+    }
+    display.innerText += digit;
+}
+
+function handleActionEvent(event) {
+    let action = event.target.innerText;
+    display.innerText += " " + action;
+}
+
+function handleEvaluateEvent() {
+    display.innerText = ` ${evaluate(display.innerText)}`
+}
+
+/** Helper functions */
+function clearDisplay() {
+    display.innerText = "";
+}
+
 function evaluate(string) {
     let arr = string.split(" ");
 
+    // parseint() needed for addition
     let a = parseInt(arr[0]);
     let b = parseInt(arr[2]);
     
